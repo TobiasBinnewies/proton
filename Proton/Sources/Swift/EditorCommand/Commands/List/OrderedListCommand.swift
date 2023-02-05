c0a81d164964ff21e321cced75e546eed86897f2
TobiasMBP <binnewiestobias@gmail.com>
2023-02-05 19:19:43 +0100

    Created List commands

---
+++ Proton/Sources/Swift/EditorCommand/Commands/List/OrderedListCommand.swift
@@ -0,0 +1,28 @@
+//
+//  OrderedListCommand.swift
+//  Proton
+//
+//  Created by Tobias Binnewies on 05.02.23.
+//  Copyright © 2023 Rajdeep Kwatra. All rights reserved.
+//
+
+import Foundation
+
+class OrderedListCommand: ListCommand {
+    
+    /// Name of the command
+    public override var name: CommandName {
+        return CommandName("orderedListCommand")
+    }
+    
+    override func execute(on editor: EditorView) {
+        var attributeValue: String? = "listItemValue"
+        // remove list if existing
+        if editor.contentLength > 0,
+           editor.attributedText.attribute(.listItem, at: min(editor.contentLength - 1, editor.selectedRange.location), effectiveRange: nil) != nil {
+            attributeValue = nil
+        }
+        // create / remove list
+        super.execute(on: editor, attributeValue: attributeValue)
+    }
+}
---
+++ Proton/Sources/Swift/EditorCommand/Commands/List/UnorderedListCommand.swift
@@ -0,0 +1,28 @@
+//
+//  UnorderedListCommand.swift
+//  Proton
+//
+//  Created by Tobias Binnewies on 05.02.23.
+//  Copyright © 2023 Rajdeep Kwatra. All rights reserved.
+//
+
+import Foundation
+
+class UnorderedListCommand: ListCommand {
+    
+    /// Name of the command
+    public override var name: CommandName {
+        return CommandName("unorderedListCommand")
+    }
+    
+    override func execute(on editor: EditorView) {
+        var attributeValue: String? = "listItemValue"
+        // remove list if existing
+        if editor.contentLength > 0,
+           editor.attributedText.attribute(.listItem, at: min(editor.contentLength - 1, editor.selectedRange.location), effectiveRange: nil) != nil {
+            attributeValue = nil
+        }
+        // create / remove list
+        super.execute(on: editor, attributeValue: attributeValue)
+    }
+}
