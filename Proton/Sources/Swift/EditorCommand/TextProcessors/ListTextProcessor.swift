347d7d098368a1c62a65a851d37b797695afa034
TobiasMBP <binnewiestobias@gmail.com>
2023-02-09 18:54:17 +0100

    fixed last commit

--- Proton/Sources/Swift/EditorCommand/TextProcessors/ListTextProcessor.swift
+++ Proton/Sources/Swift/EditorCommand/TextProcessors/ListTextProcessor.swift
@@ -78,7 +78,7 @@ public class ListTextProcessor: TextProcessing {
             
 //            editor.replaceCharacters(in: editedRange, with: ListTextProcessor.blankLineFiller)
             
-            createListItemInANewLine(editor: editor, editedRange: editedRange, attributeValue: attributedValue)
+            createListItemInANewLine(editor: editor, editedRange: editedRange.nextPosition, attributeValue: attributedValue)
             
             
             // TODO: Insert ShiftReturn and Exit List
